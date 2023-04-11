b8dcfa828984ea384ce5ebef70ca286866e4c8be
tangyapeng <tangyapeng@haier.com>
2023-04-11 13:42:03 +0800

    to add c and cpp mix call codes

---
+++ C_CPP_MIX/Display.cpp
@@ -0,0 +1,16 @@
+#include "Display.h"
+#include <stdio.h>
+#include <iostream>
+
+Display::Display(/* args */)
+{
+}
+
+Display::~Display()
+{
+}
+
+void Display::display_string(const char * string)
+{
+    
+}
---
+++ C_CPP_MIX/Display.h
@@ -0,0 +1,17 @@
+#ifndef __DISPLAY_H__
+#define __DISPLAY_H__
+
+class Display
+{
+private:
+    /* data */
+public:
+    Display(/* args */);
+    ~Display();
+    void display_string(const char * string);
+};
+
+
+
+
+#endif
